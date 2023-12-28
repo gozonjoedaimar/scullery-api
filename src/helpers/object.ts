8e01d0eaaf5e679b849467733586d3325bc135c7
gozonjoedaimar <gozonjoedaimar@gmail.com>
2023-12-28 09:25:41 +0800

    Do not use type Object

--- src/helpers/object.ts
+++ src/helpers/object.ts
@@ -1,4 +1,4 @@
-export const ob_null = function (ob: Object) {
+export const ob_null = (ob: {[key: string]: string|number|null|undefined}) => {
 	if (Object.keys(ob).length === 0) {
 		return null;
 	}
